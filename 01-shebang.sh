9c8d42baccb9af260e3756370f8c07bee6a90085
D <saiprasath.d@capgemini.com>
2023-02-07 20:32:27 +0530

    1 bg correct

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e "\e [41;31m Hello World \e[0m \n \e[44;32m Welcome DevOps Learning \e[0m \n \e[47;36m Started from Linux \e[0m"
+echo -e "\e[41;31m Hello World \e[0m \n \e[44;32m Welcome DevOps Learning \e[0m \n \e[47;36m Started from Linux \e[0m"
