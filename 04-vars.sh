97a4a4d874eeea63dfdcf351078af19cebe6b7ef
D <saiprasath.d@capgemini.com>
2023-02-07 20:37:49 +0530

    var-1

---
+++ 04-vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+a=100
+#a is 10 and integer
+
+b=xyz
+# abc is string
+
+### No datatypes in BASH screpting by default all are string
+
+
+echo Value of a : $a
+
+echo Value of b: $b
+
+echo $a:$b
+
