cfcc4f550198309139120ed0a19cdbedc992a963
D <saiprasath.d@capgemini.com>
2023-02-07 20:26:38 +0530

    added colors

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e "Hello World \nWelcome DevOps Learning\nStarted from Linux"
+echo -e "\e [25m Hello World \e[0m \n \e[32m Welcome DevOps Learning \e[0m \n \e[20m Started from Linux \e[0m"
