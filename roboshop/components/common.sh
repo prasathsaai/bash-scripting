cd9af76c0ec912e3aa4b631e6f4191be929b9c9b
D <saiprasath.d@capgemini.com>
2023-02-09 20:51:59 +0530

    adding makefile-3

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,7 +1,6 @@
-# Validating the excuting user is root or not
-
-ID = $(id -u)
-if [ $ID -ne 0 ]; then
-    echo -e "\e[31m Try executing scripts with SUDO or ROOT user \e[0m"
+# validating whether the executing user is root or not
+ID=$(id -u)
+if [ $ID -ne 0 ]; then 
+    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
     exit 1
 fi
