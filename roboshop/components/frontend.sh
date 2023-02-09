55093cb37f207ca8a836ad6bde5172b4e25af57b
D <saiprasath.d@capgemini.com>
2023-02-09 21:11:28 +0530

    adding frontend-log

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,15 +2,15 @@
 
 source components/common.sh  # Validating the excuting user is root or not
 
-echo ">>> \e[43;36m Installing Frontend \e[0m <<<"
+echo -e ">>> \e[41;36m Installing Frontend \e[0m <<<"
 
-yum install nginx -y
+yum install nginx -y >> /tmp/frontend.log
 systemctl enable nginx
 systemctl start nginx
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 cd /usr/share/nginx/html
 rm -rf *
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip >> /tmp/frontend.log
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo ">>> /e[43;36m Installing MongoDB /e[0m <<<"
+source components/common.sh  # Validating the excuting user is root or not
+
+echo -e ">>> \e[41;36m Installing Frontend \e[0m <<<"
+
+#Downloading MongoDB server
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+
