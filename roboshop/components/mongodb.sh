1e746f163a3bd3e26ff7d2d1e27c6ea7cfcc52d3
D <saiprasath.d@capgemini.com>
2023-02-10 13:55:32 +0530

    adding mongodb-04

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -31,7 +31,8 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/${COMPONE
 stat $?
 
 echo -n "Extracting Schema File:"
-cd /tmp && unzip mongodb.zip
+cd /tmp 
+unzip mongodb.zip -y >> /tmp/${COMPONENT}.log
 stat $?
 
 echo -n "Injecting Schema to ${COMPONENT}:"
