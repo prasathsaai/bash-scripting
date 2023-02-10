0c2c93358fedf31a0c26aa4ff87409e7bfa70838
D <saiprasath.d@capgemini.com>
2023-02-10 21:11:38 +0530

    adding -frontend proxy-5

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,48 +1,46 @@
 #!/bin/bash
-set -e # ensure your script will stop at faillur command
+set -e   # ensure your script will stop if any of the instruction fails
 
-source components/common.sh  # Validating the excuting user is root or not
+source components/common.sh
 
-echo -e ">>> \e[41;36m Installing Frontend \e[0m <<<"
+echo -n "Installing Nginx: "
+yum install nginx -y   >> /tmp/frontend.log 
 
-echo -n "Installing Nginx:"
-yum install nginx -y >> /tmp/frontend.log
+systemctl enable nginx 
+
+echo -n "Starting Nginx: "
+systemctl start nginx 
 stat $?
 
-systemctl enable nginx >> /tmp/frontend.log
-
-echo -n "Starting Nginx:"
-systemctl start nginx
-stat $?
-
-echo -n "Downloading the application files:"
+echo -n "Downloading the Code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $?
 
 cd /usr/share/nginx/html
 rm -rf *
-
-echo -n "Extracting the Code:"
+echo -n "Extracting the zip file:"
 unzip -o /tmp/frontend.zip >> /tmp/frontend.log
-stat $?
+stat $? 
 
 mv frontend-main/* .
 mv static/* .
+echo -n "Performing Cleanup: "
 rm -rf frontend-main README.md
+stat  $?
 
-echo "Performing Cleanup:"
+echo -n "Configuring the Reverse Proxy: "
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
 
 for component in catalogue user cart shipping payment; do 
     echo -n "Updating the proxy file"
-    sed -i -e "/${component}/s/localhost/${component}.awsdevops.internal/"  /etc/nginx/default.d/roboshop.conf
+    sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
     stat $?
 done
 
-cd /etc/nginx/default.d/ 
-systemctl daemon-reload
-
-echo -n "Restarting Nginx:"
+echo -n "Starting Ngnix: "
 systemctl restart nginx
 stat $?
+
+# source is a command to import a file and run it locally
