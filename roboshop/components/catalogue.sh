c588beea0e39cfca8cb50279a196b57eaf0b8b3d
D <saiprasath.d@capgemini.com>
2023-02-10 14:37:18 +0530

    adding catalogue-04

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -24,12 +24,13 @@ echo -n "Downloading ${COMPONENT} Application File:"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
-echo -n "Cleanup of old ${COMPONENT}:"
+echo "Cleanup of old ${COMPONENT}:"
 rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log
 
 echo -n "Extracting ${COMPONENT} files:"
 cd /home/${FUSER}
-unzip -o /tmp/${COMPONENT}.zip && mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log
+unzip -o /tmp/${COMPONENT}.zip >> /tmp/${COMPONENT}.log
+mv ${COMPONENT}-main ${COMPONENT}
 stat $?
 
 echo -n "Changing the Ownership to $FUSER:"
