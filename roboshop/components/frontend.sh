66a9ae8d14294d6ea4fb8c9b7a7cf10f3034660d
D <saiprasath.d@capgemini.com>
2023-02-10 13:43:42 +0530

    adding mongodb-01

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,30 +6,33 @@ source components/common.sh  # Validating the excuting user is root or not
 echo -e ">>> \e[41;36m Installing Frontend \e[0m <<<"
 
 echo -n "Installing Nginx:"
-
 yum install nginx -y >> /tmp/frontend.log
 stat $?
 
-systemctl enable nginx
+systemctl enable nginx >> /tmp/frontend.log
 
 echo -n "Starting Nginx:"
-
 systemctl start nginx
-
 stat $?
 
 echo -n "Downloading the application files:"
-
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
 stat $?
 
 cd /usr/share/nginx/html
 rm -rf *
+
+echo -n "Extracting the Code:"
 unzip -o /tmp/frontend.zip >> /tmp/frontend.log
+stat $?
+
 mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
+
+echo -n "Performing Cleanup:"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
+echo -n "Restarting Nginx:"
 systemctl restart nginx
+stat $?
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,12 +3,35 @@ set -e # ensure your script will stop at faillur command
 
 source components/common.sh  # Validating the excuting user is root or not
 
+COMPONENT=mongodb
+
 echo -e ">>> \e[41;36m Installing Frontend \e[0m <<<"
 
 # Downloading MongoDB server
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+echo -n "Configuring the ${COMPONENT} repo:"
+curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
 
 # Installing MongoDB
-yum install -y mongodb-org
-systemctl enable mongod
+echo -n "Installing ${COMPONENT} Server:"
+yum install -y mongodb-org >> /tmp/${COMPONENT}.log
+stat $?
+
+echo -n "Updating ${COMPONENT} Config:"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+stat $?
+
+echo -n "Starting ${COMPONENT} Service:"
+systemctl enable mongod >> /tmp/${COMPONENT}.log
 systemctl start mongod
+stat $?
+
+echo -n "Downloading ${COMPONENT} schema:"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $?
+
+echo -n "Injecting Schema to ${COMPONENT} "
+cd /tmp && unzip mongodb.zip && cd mongodb-main
+mongo < catalogue.js # Injecting Catalogue Table
+mongo < users.js # Injecting User Table
+stat $?
