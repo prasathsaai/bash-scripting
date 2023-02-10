fe95dc72f437acf3396649f954b415773e296743
D <saiprasath.d@capgemini.com>
2023-02-10 15:25:54 +0530

    adding catalogue-12

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -9,7 +9,7 @@ FUSER=roboshop
 echo -e ">>> \e[41;36m Installing Frontend \e[0m <<<"
 
 echo -n "Configureing yum repo for NodeJS:"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash >> /tmp/${COMPONENT}.log
+curl -sL https://rpm.nodesource.com/setup_16.x | bash >> /tmp/${COMPONENT}.log
 stat $?
 
 echo -n "Installing NodeJS:"
