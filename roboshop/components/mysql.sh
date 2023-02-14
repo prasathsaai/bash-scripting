b870b1e6d67fda7781d3b120486eaceb7e6085b4
D <saiprasath.d@capgemini.com>
2023-02-14 17:28:14 +0530

    adding -mysql-5

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-set -e # ensure your script will stop at faillur command
 
-source components/common.sh  # Validating the excuting user is root or not
+source components/common.sh
 
 COMPONENT=mysql
-LOGFILE=robot.log
-MYSQL_PASSWORD=RoboShop@1
+LOGFILE=/tmp/robot.log 
+MYSQL_PASSWORD="RoboShop@1"
 
-echo -n "Configuring the ${COMPONENT} repo: "
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
-stat $?
+echo -n "Configuring the $COMPONENT repo: "
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/${COMPONENT}.repo &>> ${LOGFILE}
+stat $? 
 
-# Installing MySQL
-echo -n "Installing ${COMPONENT} Server: "
+echo -n "Installing $COMPONENT :"
 yum install mysql-community-server -y &>> ${LOGFILE}
-stat $?
+stat $? 
 
-echo -n "Starting ${COMPONENT} Service: "
-systemctl enable mysqld &>> ${LOGFILE}
+echo -n "Starting ${COMPONENT} : "
+systemctl enable mysqld  &>> ${LOGFILE}
 systemctl start mysqld &>> ${LOGFILE}
-stat $?
+stat $? 
 
-echo -n "Fletching the default root password: "
-DEFAULT_ROOT_PASSWORD=$( sudo grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}')
-stat $?
+
+echo -n "Fetching the default root password: "
+DEFAULT_ROOT_PASSWORD=$(sudo grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}')
+stat $? 
 
 #If the exit code is non-zero then only I want to execute, if not, I would like to skip 
 echo show databases | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
@@ -33,9 +32,9 @@ if [ $? -ne 0 ]; then
     stat $? 
 fi 
 
-echo 'show plugins;' | mysql -uroot -pRoboShop@1 &>> ${LOGFILE} | grep validate_password &>> ${LOGFILE}
-if [ $? -eq 0 ] ; then
-    echo -n "Uninstall the password validate plugin: "
-    echo "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
-    stat $?
-fi
+echo 'show plugins;' | mysql -uroot -pRoboShop@1 | grep validate_password &>> ${LOGFILE}
+if [ $? -eq 0 ] ; then 
+    echo -n "Uninstalling the password validate plugin :"
+    echo  "uninstall plugin validate_password;" | mysql -uroot -pRoboShop@1  &>> ${LOGFILE}
+    stat $? 
+fi 
