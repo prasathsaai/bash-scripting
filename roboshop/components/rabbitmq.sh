8fcddcde77f5df031fa978e93365abf6933c020c
sai <prasathsaai@gmail.com>
2023-02-15 20:14:41 +0530

    rabbitmq-06

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -11,7 +11,7 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 stat $? 
 
 echo -n "Installing RabbitMQ: "
-yum install rabbitmq -y  &>> ${LOGFILE} 
+yum install rabbitmq-server -y  &>> ${LOGFILE} 
 stat $? 
 
 echo -n "Starting $COMPONENT :"
