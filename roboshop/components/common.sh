ead54d6dd6658eba95fe00c9282edc50d6312fe5
sai <prasathsaai@gmail.com>
2023-02-15 20:21:30 +0530

    payment-02

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -99,3 +99,26 @@ MAVEN () {
     CONFIG_SVC
 
 }
+
+PYTHON() {
+    echo -n "Installing Pyhton:"
+    yum install python36 gcc python3-devel -y &>> ${LOGFILE} 
+    stat $? 
+
+    USER_SETUP
+
+    DOWNLOAD_AND_EXTRACT
+
+    cd /home/${FUSER}/${COMPONENT}/
+    pip3 install -r requirements.txt   &>> ${LOGFILE} 
+    stat $? 
+
+    USER_ID=$(id -u roboshop)
+    GROUP_ID=$(id -g roboshop)
+
+    echo -n "Updating the $COMPONENT.ini file"
+    sed -i -e "/^uid/ c uid=${USER_ID}" -e "/^gid/ c gid=${GROUP_ID}" payment.ini
+    stat $? 
+
+    CONFIG_SVC
+}
